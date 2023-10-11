fa3475f315bb804d1a2cf2685eab3595bc26413b
Timofey Goloshchapov <tim.goloschapov@gmail.com>
2023-10-11 17:22:47 -0500

    slug bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import { getProjects } from "@/sanity/sanity-utils"
 import Image from "next/image";
+import Link from "next/link";
+
+
 
 export default async function Home() {
   const projects = await getProjects();
   return (
     <div className="max-w-5xl mx-auto">
-      <h1 className="text-7xl font-extrabold py-20">Hello, I am <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">Timmy</span></h1>
+      <h1 className="text-7xl font-extrabold py-20">Hello, I am <span className="bg-gradient-to-r from-yellow-400 via-lime-500 to-green-600 bg-clip-text text-transparent">Timmy</span></h1>
       <p className="mt-3 text-x1 text-lime-600">Hi hello hey</p>
       <h2 className="mt-24 font-bold text-blue-700 text-3x1">My Projects</h2>
       <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
-          <div key={project._id} className="border border-yellow-500 rounded-lg p-3">
+          <Link href={`/projects/${project.slug}`} key={project._id} className="border-2 border-yellow-500 rounded-lg p-3 hover:scale-105 hover:border-blue-500 transition">
             {project.image && (
               <Image
                 src={project.image}
@@ -23,7 +26,7 @@ export default async function Home() {
             <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
               {project.name}
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,19 +1,20 @@
 // all functions to use to grab data
 
+import { Project } from "@/types/project";
 import { createClient, groq } from "next-sanity";
 
-export async function getProjects(){
-    // use query language GROQ 
-    const client = createClient({
-        //with client, we can read data from studio
-        projectId: "vr6otv1n",
-        dataset: "production",
-        apiVersion: "2023-09-26",
-    });
+export async function getProjects(): Promise<Project[]> {
+  // use query language GROQ 
+  const client = createClient({
+    //with client, we can read data from studio
+    projectId: "vr6otv1n",
+    dataset: "production",
+    apiVersion: "2023-09-26",
+  });
 
-    return client.fetch(
-        //groq query
-        groq`*[_type == "project"]{
+  return client.fetch(
+    //groq query
+    groq`*[_type == "project"]{
             _id,
             _createdAt,
             name,
@@ -22,5 +23,28 @@ export async function getProjects(){
             url,
             content
         }`
-    )
+  )
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    //with client, we can read data from studio
+    projectId: "vr6otv1n",
+    dataset: "production",
+    apiVersion: "2023-09-26",
+  });
+
+  return client.fetch(
+    //groq query
+    groq`*[_type == "project" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`,
+    { slug: slug ?? '' }
+  )
 }
