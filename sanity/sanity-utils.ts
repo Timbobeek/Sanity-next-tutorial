a847c83d2e7044950e5e12a6c2d63dcc926ebddd
Timofey Goloshchapov <tim.goloschapov@gmail.com>
2023-10-12 00:51:04 -0500

    pages done

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -3,6 +3,7 @@
 import { Project } from "@/types/project";
 import { createClient, groq } from "next-sanity";
 import clientConfig from "./schemas/config/client-config";
+import { Page } from "@/types/page";
 
 
 export async function getProjects(): Promise<Project[]> {
@@ -36,4 +37,30 @@ export async function getProject(slug: string): Promise<Project> {
     }`,
     { slug: slug ?? '' }
   )
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+    }`
+  )
+
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+      content
+    }`,
+    { slug }
+  )
+
 }
